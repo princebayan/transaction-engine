37c2fdbc2108c3fc0043b02588f5dd5910d18204
Amir <amir.bayan@saradarbank.com>
2022-08-21 19:10:52 +0300

    feat: add validation for the execute request

--- src/main/java/com/example/transactionengine/transaction/request/ExecuteRequest.java
+++ src/main/java/com/example/transactionengine/transaction/request/ExecuteRequest.java
@@ -1,6 +1,8 @@
 package com.example.transactionengine.transaction.request;
 
 import java.math.BigDecimal;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -10,12 +12,20 @@ import lombok.ToString;
 @ToString
 public class ExecuteRequest {
 
+  @Positive
   private int customerId;
+  @NotBlank
   private String sourceAccount;
+  @NotBlank
   private String destinationAccount;
+  @Positive
   private BigDecimal amount;
+  @Positive
   private BigDecimal totalDebitedFromSource;
+  @Positive
   private BigDecimal totalCreditedToDestination;
+  @NotBlank
   private String sourceCurrency;
+  @NotBlank
   private String destinationCurrency;
 }
